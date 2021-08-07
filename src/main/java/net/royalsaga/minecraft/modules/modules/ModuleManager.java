71442524239c6ec28000f245ee6f5f3cc8b815ea
iGabyTM <contactgabytm@gmail.com>
2021-08-07 20:49:55 +0300

    Reformat license comment

--- src/main/java/net/royalsaga/minecraft/modules/modules/ModuleManager.java
+++ src/main/java/net/royalsaga/minecraft/modules/modules/ModuleManager.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- *
+ * <p>
  * Copyright (c) 2021 RoyalSaga
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -53,12 +53,12 @@ public class ModuleManager<P extends JavaPlugin> {
         module.getListeners().forEach(it -> Bukkit.getServer().getPluginManager().registerEvents(it, plugin));
 
         if (module instanceof CommandModule) {
-           final CommandModule commandModule = (CommandModule) module;
+            final CommandModule commandModule = (CommandModule) module;
 
-           commandModule.getCompletions().forEach(commandManager.getCompletionHandler()::register);
-           commandModule.getMessages().forEach(commandManager.getMessageHandler()::register);
-           commandModule.getParameters().forEach(commandManager.getParameterHandler()::register);
-           commandModule.getCommands().forEach(commandManager::register);
+            commandModule.getCompletions().forEach(commandManager.getCompletionHandler()::register);
+            commandModule.getMessages().forEach(commandManager.getMessageHandler()::register);
+            commandModule.getParameters().forEach(commandManager.getParameterHandler()::register);
+            commandModule.getCommands().forEach(commandManager::register);
         }
 
         if (module instanceof PlaceholderProvider) {
